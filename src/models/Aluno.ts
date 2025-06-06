2e876160259207e636f7b4771a9ab4ff0f5225cf
JoaoAndrade-dev <joao.andrade1610@outlook.com>
2025-06-06 16:22:49 -0300

    feat: Add Service, AuthContext, models

---
+++ src/models/Aluno.ts
@@ -0,0 +1,13 @@
+import { type Treino } from "./Treino";
+import { type Usuario } from "./Usuario";
+
+export interface Aluno {
+  id: number;
+  nome: string;
+  endereco: string;
+  telefone: string;
+  peso: number;
+  altura: number;
+  treino?: Treino;
+  usuario?: Usuario;
+}
